0353b9e11676d5277f37848f8536d6ba15ae8c1e
Yogendra Maurya <znbssnsn@gmail.com>
2025-08-15 21:10:52 +0530

    Delete run-server.bat

--- run-server.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Starting Chat Server...
-echo.
-echo Make sure MySQL is running and database.properties is configured correctly.
-echo.
-mvn exec:java -Dexec.mainClass="com.chatapp.server.ChatServer"
-pause
